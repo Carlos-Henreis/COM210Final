1fb702a3f7da9db43202fcf0d74c9b5067d15c2f
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2016-06-03 21:22:31 -0300

    Problema da rota atendente corrigido

---
+++ CoopAgro/storage/framework/views/9029d3858e82e52b9091fb30f5b485dab7ff0b93.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CoopAgro-Area Presidente</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <?php /* <link href="<?php echo e(elixir('css/app.css')); ?>" rel="stylesheet"> */ ?>
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+        .rodape {    
+            position:absolute;   
+             bottom:0;    
+             width:100%;    
+             height:30px;       
+             background:#e7e7e7; 
+         }
+
+    </style>
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a href="<?php echo e(url('/')); ?>">
+                    <img src="../logo.png" width="100" height="50">
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <?php echo e(auth()->guard('presidente')->user()->name); ?> <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="<?php echo e(url('presidente/logout')); ?>"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                </ul>
+            </div>
+        
+        </div>
+
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Seja Bem vindo <?php echo e(auth()->guard('presidente')->user()->name); ?></h3><p><h4>Menu Principal</h4></p></div>
+
+                    <div class="panel-body">
+                        <ul>
+                            <li>Funcionários</li>
+                                <ul>
+                                    <li><a href="#">Técnicos</a></li>
+                                    <li><a href="atendente/cruds">Atendentes</a></li>
+                                </ul>
+                            <li><a href="">Patrimônios</a></li>
+                            <li><a href="#">Relatórios</a></li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="rodape"><div class="nav navbar-nav navbar-right">
+
+        <img src="../ss.jpg" width="90" height="25">SWFactory Consultoria e Sistemas Ltda - <a href="http://www.swfactory.com.br">http://www.swfactory.com.br</a></div>
+
+    </div>
+
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <?php /* <script src="<?php echo e(elixir('js/app.js')); ?>"></script> */ ?>
+</body>
+</html>
