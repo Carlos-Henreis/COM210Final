aaf53f8196514dd5f9fab5d75ba50f13d677296d
David Batista <david.batista3010@gmail.com>
2016-06-17 18:22:59 -0300

    Adicionando mensagens de sucesso
    Associado

--- CoopAgro/resources/views/associado/crud/index.blade.php
+++ CoopAgro/resources/views/associado/crud/index.blade.php
@@ -31,4 +31,7 @@
         </tbody>
     </table>
 </div>
+@if(Session::has('flash_message'))
+    <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message') !!}</em></div>
+@endif
 @endsection
